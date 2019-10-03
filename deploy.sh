3dc0bc761c9c4798da9a31797a2278ee587046af
Platon Efimov <platon.efimov@gmail.com>
2019-10-04 01:00:00 +0700

    Adds Frontend Deploying

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cat ormconfig.prod.json > ormconfig.json
+test -f ormconfig.json || cat ormconfig.prod.json > ormconfig.json
 
 docker-compose down
 
-docker-compose build foosball-rating
+docker-compose build foosball-rating-backend
 
 docker-compose up -d
