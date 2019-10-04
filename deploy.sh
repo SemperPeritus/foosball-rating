4056246964bddd6a94b39ecf2db2aaaf4e245abf
Platon Efimov <platon.efimov@gmail.com>
2019-10-04 16:54:06 +0700

    Adds Home and Players on Frontend

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 test -f ormconfig.json || cat ormconfig.prod.json > ormconfig.json
+test -f .env || cat .env.prod > .env
 
 docker-compose down
 
 docker-compose build foosball-rating-backend
+docker-compose build foosball-rating-frontend
 
 docker-compose up -d
---
+++ frontend/src/helpers/api.js
@@ -0,0 +1,49 @@
+const base = `http://159.69.217.124:3000`;
+
+function send({ method, path, data, token }) {
+  const fetch = process.browser ? window.fetch : require('node-fetch').default;
+
+  const opts = { method, headers: {} };
+
+  if (data) {
+    opts.headers['Content-Type'] = 'application/json';
+    opts.body = JSON.stringify(data);
+  }
+
+  if (token) {
+    opts.headers['Authorization'] = `Token ${token}`;
+  }
+
+  return fetch(`${base}/${path}`, opts)
+    .then(r => r.text())
+    .then(json => {
+      try {
+        return JSON.parse(json);
+      } catch (err) {
+        return json;
+      }
+    });
+}
+
+function get(path, token) {
+  return send({ method: 'GET', path, token });
+}
+
+function del(path, token) {
+  return send({ method: 'DELETE', path, token });
+}
+
+function post(path, data, token) {
+  return send({ method: 'POST', path, data, token });
+}
+
+function put(path, data, token) {
+  return send({ method: 'PUT', path, data, token });
+}
+
+export const api = {
+  get,
+  del,
+  post,
+  put,
+};
