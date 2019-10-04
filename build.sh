6cd8a8f2bc2789f393b58ba2b762428281712a10
Platon Efimov <platon.efimov@gmail.com>
2019-10-05 05:59:44 +0700

    Adds build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+test -f ormconfig.json || cat ormconfig.prod.json > ormconfig.json
+
+docker-compose build foosball-rating-backend
+docker-compose build foosball-rating-frontend
