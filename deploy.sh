00cd34659835becd23687f6286ecb64c167300f7
Platon Efimov <platon.efimov@gmail.com>
2019-10-05 23:26:18 +0700

    Fixes deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 test -f ormconfig.json || cat ormconfig.prod.json > ormconfig.json
-test -f frontend/.env || cat frontend/.env.prod > frontend/.env
-
-docker-compose down
 
 docker-compose build foosball-rating-backend
 docker-compose build foosball-rating-frontend
