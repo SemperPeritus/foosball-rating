a1406f9e2622c633c8fad3c5bbd338fa04dfb7ff
Platon Efimov <platon.efimov@gmail.com>
2019-10-07 22:40:32 +0700

    Adds Player page

--- frontend/src/components/Navigation.svelte
+++ frontend/src/components/Navigation.svelte
@@ -53,9 +53,5 @@
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>Главная</a></li>
 		<li><a class='{segment === "player" ? "selected" : ""}' href='player'>Игроки</a></li>
 		<li><a class='{segment === "game" ? "selected" : ""}' href='game'>Игры</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
 	</ul>
 </nav>
---
+++ frontend/src/routes/player/[id].svelte
@@ -0,0 +1,49 @@
+<script context="module">
+  import { api } from '../../helpers/api';
+
+  export async function preload({ params }) {
+    const player = await api.get(`player/${params.id}/?include=games,games.firstTeam,games.secondTeam`);
+    return { player };
+  }
+</script>
+
+<script>
+  export let player;
+
+  const renderPlayers = players => players.map(player => player.name).join(', ');
+</script>
+
+<style>
+  .rating {
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .winners {
+    color: blue;
+    font-weight: bold;
+  }
+</style>
+
+<svelte:head>
+  <title>Игрок {player.name}</title>
+</svelte:head>
+
+<h1>{player.name}</h1>
+
+<div class="rating">Рейтинг: {player.rating}</div>
+
+<h2>История игр ({player.games.length})</h2>
+
+<div>
+{#each player.games as { id, firstTeam, secondTeam, winner, created }}
+    <p>
+    <b>{id}</b>
+    |
+    <span class={winner === 1 ? 'winners' : ''}>{renderPlayers(firstTeam)}</span>
+    vs
+    <span class={winner === 2 ? 'winners' : ''}>{renderPlayers(secondTeam)}</span>
+    | Дата: {new Date(created).toLocaleDateString('ru-RU')}
+    </p>
+{/each}
+</div>
