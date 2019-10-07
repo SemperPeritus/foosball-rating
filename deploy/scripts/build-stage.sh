23f9c3d66f08b34a112330a022a6011821b7e925
Platon Efimov <platon.efimov@gmail.com>
2019-10-07 12:56:00 +0700

    Changes API Host

--- deploy/scripts/build-stage.sh
+++ deploy/scripts/build-stage.sh
@@ -2,7 +2,7 @@
 
 test -f .env || cat deploy/env/env.stage > .env
 test -f deploy/env/PostgreSQL.env || cat deploy/env/PostgreSQL.env.prod > deploy/env/PostgreSQL.env
-test -f deploy/env/backend.env || cat deploy/env/backend.env.prod > deploy/env/backend.env
+test -f deploy/env/backend.env || cat deploy/env/backend.env.stage > deploy/env/backend.env
 test -f deploy/env/frontend.env || cat deploy/env/frontend.env.stage > deploy/env/frontend.env
 
 test -f ormconfig.json || cat deploy/static-configs/ormconfig.stage.json > ormconfig.json
