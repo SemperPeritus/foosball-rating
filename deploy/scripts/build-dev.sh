01137c0f0a6dc9ca9e79a74ddcc8a68cef1a6506
Andrew <ndrwbv@gmail.com>
2019-10-07 14:31:16 +0700

    Adds project description

--- deploy/scripts/build-dev.sh
+++ deploy/scripts/build-dev.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 test -f ormconfig.json || cat deploy/static-configs/ormconfig.dev.json > ormconfig.json
+docker run -d --name foosball-rating-psql -p 9219:5432 -e POSTGRES_PASSWORD=zero -e POSTGRES_USER=zero -e POSTGRES_DB=foosball-rating postgres
 
 #docker-compose build foosball-rating-backend
 #docker-compose build foosball-rating-frontend
