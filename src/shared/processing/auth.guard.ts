f5ab28032c5135457df16ad9d5452c8756a02ca1
Platon Efimov <platon.efimov@gmail.com>
2019-10-10 11:30:16 +0700

    Modifies auth guard

--- src/shared/processing/auth.guard.ts
+++ src/shared/processing/auth.guard.ts
@@ -1,36 +1,44 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import * as jwt from 'jsonwebtoken';
+import { createQueryBuilder } from 'typeorm';
+import { UserEntity } from '../../modules/user/user.entity';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const roles = this.reflector.get<string[] | number>('roles', context.getHandler());
-
-    const request = context.switchToHttp().getRequest();
-    const user = await this.getUser(request.headers.token);
+    const roles = this.reflector.get<number[] | number>('roles', context.getHandler());
 
     if (!roles) {
       return true;
     }
 
+    const request = context.switchToHttp().getRequest();
+    const userId = await this.getUserId(request.headers.token);
+
+    const user = await createQueryBuilder(UserEntity, 'user')
+      .where('user.id = :id', { id: userId })
+      .getOne();
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
+    }
     if (typeof roles === 'number') {
       return user.role >= roles;
     }
     if (Array.isArray(roles)) {
       return roles.indexOf(user.role) !== -1;
     }
-
-    throw new UnauthorizedException(user.message || user.name);
   }
 
-  async getUser(token: string) {
+  async getUserId(token: string): Promise<number> {
     try {
-      return jwt.verify(token, process.env.AUTH_SECRET);
+      const user = await jwt.verify(token, process.env.AUTH_SECRET);
+      return Number.parseInt((user as any).id, 10);
     } catch (error) {
-      return error;
+      throw new UnauthorizedException(error.message || error.name);
     }
   }
 }
