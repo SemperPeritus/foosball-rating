86a277362e20ab957eff8d758e338659fe7e59ea
Platon Efimov <platon.efimov@gmail.com>
2019-10-10 16:59:28 +0700

    Fixes DB backup script

--- deploy/scripts/prod-utils/backup-db.sh
+++ deploy/scripts/prod-utils/backup-db.sh
@@ -1,2 +1,2 @@
-cd home/player/foosball-rating
+cd /home/player/foosball-rating
 /usr/local/bin/docker-compose exec -T PostgreSQL pg_dump -U zero -Fc foosball-rating > /home/player/foosball-rating-backups/backup_`date +%d-%m-%Y"_"%H_%M_%S`.dump 2>/tmp/cron-backup-db.errors.log
