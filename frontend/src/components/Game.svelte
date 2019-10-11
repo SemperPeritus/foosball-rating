286fa8f63312ed3fdf359ceceea630df87480b69
Platon Efimov <platon.efimov@gmail.com>
2019-10-11 15:26:26 +0700

    Adds Game and GameList components to front

---
+++ frontend/src/components/Game.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let game;
+  export let highlightedPlayerId;
+</script>
+
+<style>
+  .game {
+    margin-top: 10px;
+  }
+
+  .game__winners {
+    color: blue;
+    font-weight: bold;
+  }
+
+  .game__highlighted-player {
+    background-color: coral;
+  }
+</style>
+
+<div class="game">
+  <b>{game.id}</b>
+  |
+  <span class:game__winners={game.winner === 1}>
+    {#each game.firstTeam as { id, firstName, secondName }, index}
+      <span class:game__highlighted-player={highlightedPlayerId === id}>{secondName} {firstName}</span>
+      {index === game.firstTeam.length - 1 ? '' : ', '}
+    {/each}
+  </span>
+  vs
+  <span class:game__winners={game.winner === 2}>
+    {#each game.secondTeam as { id, firstName, secondName }, index}
+      <span class:game__highlighted-player={highlightedPlayerId === id}>{secondName} {firstName}</span>
+      {index === game.secondTeam.length - 1 ? '' : ', '}
+    {/each}
+  </span>
+  | Дата: {new Date(game.created).toLocaleDateString('ru-RU')}
+</div>
--- frontend/src/routes/game/index.svelte
+++ frontend/src/routes/game/index.svelte
@@ -1,6 +1,8 @@
 <script>
   import { onMount } from 'svelte';
 
+  import GameList from '../../components/GameList.svelte';
+
   import { api } from '../../helpers/api';
   import { store } from '../../helpers/store';
 
@@ -24,18 +26,11 @@
     isLoading = true;
     await fetchGames();
   };
-
-  const renderPlayers = players => players.map(player => `${player.secondName} ${player.firstName}`).join(', ');
 </script>
 
 <style>
-  .winners {
-    color: blue;
-    font-weight: bold;
-  }
-
-  .current-player {
-    background-color: coral;
+  .game-list {
+    margin-top: 20px;
   }
 </style>
 
@@ -50,33 +45,6 @@
   <button>Создать игру</button>
 </a>
 
-<div>
-  {#if games && !isLoading}
-    {#each games as { id, firstTeam, secondTeam, winner, created }}
-      <p>
-        <b>{id}</b>
-        |
-        <span class={winner === 1 ? 'winners' : ''}>
-          {#each firstTeam as { id, firstName, secondName }, index}
-            <span class={user && user.player && user.player.id === id ? 'current-player' : ''}>
-              {secondName} {firstName}
-            </span>
-            {index === firstTeam.length - 1 ? '' : ', '}
-          {/each}
-        </span>
-        vs
-        <span class={winner === 2 ? 'winners' : ''}>
-          {#each secondTeam as { id, firstName, secondName }, index}
-            <span class={user && user.player && user.player.id === id ? 'current-player' : ''}>
-              {secondName} {firstName}
-            </span>
-            {index === secondTeam.length - 1 ? '' : ', '}
-          {/each}
-        </span>
-        | Дата: {new Date(created).toLocaleDateString('ru-RU')}
-      </p>
-    {/each}
-  {:else}
-    <h2>Loading...</h2>
-  {/if}
+<div class="game-list">
+  <GameList {games} {isLoading} highlightedPlayerId={user && user.player && user.player.id} />
 </div>
