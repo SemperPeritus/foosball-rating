4c1c08bb924941068ac1dadc51a35ff33707a69b
Platon Efimov <platon.efimov@gmail.com>
2019-10-11 17:01:15 +0700

    Adds PlayerLink component

---
+++ frontend/src/components/PlayerLink.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let player;
+  export let highlightedPlayerId;
+</script>
+
+<style>
+  .player {
+    margin-top: 8px;
+  }
+
+  .player_highlighted {
+    background-color: coral;
+  }
+</style>
+
+<div class="player">
+  <a href={`/player/${player.id}`}>
+    <span class:player_highlighted={player.id === highlightedPlayerId}>
+      {`${player.secondName} ${player.firstName}`}
+    </span>
+  </a>
+  - {Math.round(player.rating)}
+</div>
