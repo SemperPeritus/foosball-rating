0f51e5b6b53053c7a39a3dd428aa86dd25b02dd3
Platon Efimov <platon.efimov@gmail.com>
2019-10-14 13:03:58 +0700

    Updates layout of game list

--- frontend/src/components/Game.svelte
+++ frontend/src/components/Game.svelte
@@ -5,7 +5,18 @@
 
 <style>
   .game {
-    margin-top: 10px;
+    display: table-row;
+    padding: 10px;
+    border-bottom: 1px solid #aaa;
+  }
+
+  .game__cell {
+    display: table-cell;
+    padding: 10px;
+  }
+
+  .game__cell_id {
+    min-width: 30px;
   }
 
   .game__winners {
@@ -19,20 +30,18 @@
 </style>
 
 <div class="game">
-  <b>{game.id}</b>
-  |
-  <span class:game__winners={game.winner === 1}>
+  <b class="game__cell game__cell_id">{game.id}</b>
+  <div class="game__cell" class:game__winners={game.winner === 1}>
     {#each game.firstTeam as { id, firstName, secondName }, index}
       <span class:game__highlighted-player={highlightedPlayerId === id}>{secondName} {firstName}</span>
-      {index === game.firstTeam.length - 1 ? '' : ', '}
+      {index === game.firstTeam.length - 1 ? '' : '| '}
     {/each}
-  </span>
-  vs
-  <span class:game__winners={game.winner === 2}>
+  </div>
+  <div class="game__cell" class:game__winners={game.winner === 2}>
     {#each game.secondTeam as { id, firstName, secondName }, index}
       <span class:game__highlighted-player={highlightedPlayerId === id}>{secondName} {firstName}</span>
-      {index === game.secondTeam.length - 1 ? '' : ', '}
+      {index === game.secondTeam.length - 1 ? '' : '| '}
     {/each}
-  </span>
-  | Дата: {new Date(game.created).toLocaleDateString('ru-RU')}
+  </div>
+  <div class="game__cell">{new Date(game.created).toLocaleDateString('ru-RU')}</div>
 </div>
