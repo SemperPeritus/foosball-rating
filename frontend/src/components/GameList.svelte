f93950b8005fbf62566ac013414c54be1d1e8c43
Platon Efimov <platon.efimov@gmail.com>
2019-10-14 13:03:58 +0700

    Updates layout of game list

--- frontend/src/components/GameList.svelte
+++ frontend/src/components/GameList.svelte
@@ -6,8 +6,39 @@
   export let highlightedPlayerId;
 </script>
 
-<div>
+<style>
+  .game-list {
+    display: table;
+    border-collapse: collapse;
+    border-spacing: 10px;
+  }
+
+  .game-list__game {
+    display: table-row;
+    padding: 10px;
+    border-bottom: 2px solid #555;
+  }
+
+  .game-list__game__cell {
+    display: table-cell;
+    padding: 10px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .game-list__game__cell_id {
+    min-width: 30px;
+  }
+</style>
+
+<div class="game-list">
   {#if games && !isLoading}
+    <div class="game-list__game">
+      <div class="game-list__game__cell game-list__game__cell_id">ID</div>
+      <div class="game-list__game__cell">Первая команда</div>
+      <div class="game-list__game__cell">Вторая команда</div>
+      <div class="game-list__game__cell">Дата</div>
+    </div>
     {#each games as game}
       <Game {game} {highlightedPlayerId} />
     {/each}
