53a5d90a48725ba7968d906e34d6a9abaefd0829
Platon Efimov <platon.efimov@gmail.com>
2019-10-15 13:31:54 +0700

    Adds user list page

---
+++ frontend/src/components/UserRating.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let user;
+</script>
+
+<style>
+  .rating {
+    font-weight: bold;
+  }
+</style>
+
+{#if user && (user.player || user.playerWanted)}
+  <span class="rating">
+    ({Math.round(user.player ? user.player.rating : user.playerWanted.rating)}{user.player ? '' : '?'})
+  </span>
+{/if}
---
+++ frontend/src/constants/roles.js
@@ -0,0 +1,15 @@
+export const Role = {
+  FAG: 50,
+  NON_VERIFIED_USER: 80,
+  USER: 100,
+  MODERATOR: 300,
+  ADMIN: 700,
+};
+
+export const RolesMap = {
+  50: 'Пидор',
+  80: 'Неподтверждённый пользователь',
+  100: 'Пользователь',
+  300: 'Модератор',
+  700: 'Админ',
+};
